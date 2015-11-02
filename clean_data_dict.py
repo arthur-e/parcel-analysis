67d5e1c0f27ec77a604eb157fae839429e9e26e9
K. Arthur Endsley <endsley@umich.edu>
2015-11-02 11:21:30 -0500

    Refactored cleaning script and added README

--- clean_data_dict.py
+++ clean_data_dict.py
@@ -7,24 +7,34 @@ sed "s/  //g" ~/Downloads/temp.csv > ~/Downloads/temp1.csv
 
 import csv
 import re
+import sys
 
-regex = re.compile('^\d+$')
-results = []
+if __name__ == '__main__':
+    regex = re.compile('^\d+$')
+    results = []
 
-# Read in only those rows that have an field ID (the others are formatting errors)
-with open('/home/arthur/Downloads/RealtyTrac/DFS_3.0_Catalog_of_Elements_v1.7.csv', 'r') as stream:
-    reader = csv.reader(stream)
-    for l, row in enumerate(reader):
-        if l == 0:
-            header = row
-            continue
+    inp = '/home/arthur/Downloads/RealtyTrac/DFS_3.0_Catalog_of_Elements_v1.7.csv'
+    if len(sys.argv) > 1:
+        inp = sys.argv[1]
 
-        if regex.match(row[0]) is not None:
-            results.append(row)
+    out = '/home/arthur/Downloads/RealtyTrac/DFS_3.0_Catalog_of_Elements_v1.7_clean.csv'
+    if len(sys.argv) > 2:
+        out = sys.argv[2]
 
-# Write out only those rows with a field ID
-with open('/home/arthur/Downloads/RealtyTrac/DFS_3.0_Catalog_of_Elements_v1.7_clean.csv', 'w') as stream:
-    writer = csv.writer(stream)
-    writer.writerow(header)
-    for row in results:
-        writer.writerow(row)
+    # Read in only those rows that have an field ID (the others are formatting errors)
+    with open(inp, 'r') as stream:
+        reader = csv.reader(stream)
+        for l, row in enumerate(reader):
+            if l == 0:
+                header = row
+                continue
+
+            if regex.match(row[0]) is not None:
+                results.append(row)
+
+    # Write out only those rows with a field ID
+    with open(out, 'w') as stream:
+        writer = csv.writer(stream)
+        writer.writerow(header)
+        for row in results:
+            writer.writerow(row)
