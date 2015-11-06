8eee55f159a08263de10a5c3340c4bfdeab6b4a3
K. Arthur Endsley <endsley@umich.edu>
2015-11-06 15:12:40 -0500

    Minor changes now that layout files have been obtained for all tables

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 DBNAME=realtytrac
 OWNER=arthur
-FORECLOSURE_DATA_DICT=/home/arthur/Downloads/RealtyTrac/datadict_foreclosure.csv
+ASSESSOR_DATA_DICT=/home/arthur/Downloads/RealtyTrac/REALTYTRAC_DLP_3.0_Assessor_NO_Geo_Layout.csv
+FORECLOSURE_DATA_DICT=/home/arthur/Downloads/RealtyTrac/REALTYTRAC_DLP_3.0_Foreclosure_Layout.csv
 RECORDER_DATA_DICT=/home/arthur/Downloads/RealtyTrac/REALTYTRAC_DLP_3.0_Recorder_Layout.csv
 
 # Create the database
@@ -9,5 +10,8 @@ RECORDER_DATA_DICT=/home/arthur/Downloads/RealtyTrac/REALTYTRAC_DLP_3.0_Recorder
 # Create the record (Recorder) table
 python create_from_data_dict.py "record" $RECORDER_DATA_DICT "sr_unique_id" | sudo -u postgres psql -d $DBNAME -f -
 
+# Create the assessment (Assessor Record) table
+python create_from_data_dict.py "assessment" $ASSESSOR_DATA_DICT "sr_unique_id" | sudo -u postgres psql -d $DBNAME -f -
+
 # Create the foreclosure table
-python create_from_data_dict.py "foreclosure" $FORECLOSURE_DATA_DICT "property_id" | sudo -u postgres psql -d $DBNAME -f -
+python create_from_data_dict.py "foreclosure" $FORECLOSURE_DATA_DICT "unique_id_notice" | sudo -u postgres psql -d $DBNAME -f -
