f7eeaea48f8182744399d54d8a9539a3583f9a44
K. Arthur Endsley <endsley@umich.edu>
2016-02-19 13:49:47 -0500

    Added indexes and some SQL scripts

--- index_database.sh
+++ index_database.sh
@@ -4,8 +4,16 @@
 DBNAME=realtytrac
 
 # Indexes columns of record table
+# Indexes `sr_property_id`
 sudo -u postgres psql -d $DBNAME -c "CREATE INDEX idx_record_sr_property_id ON record USING hash (sr_property_id);"
 
-# Indexes columsn of foreclosure table
+# Indexes columns of foreclosure table
 # Indexes `sa_property_id` for faster lookups on Assessor property ID
 sudo -u postgres psql -d $DBNAME -c "CREATE INDEX idx_foreclosure_sa_property_id ON foreclosure USING hash (sa_property_id);"
+sudo -u postgres psql -d $DBNAME -c "CREATE INDEX idx_foreclosure_recording_date ON foreclosure USING btree (recording_date);"
+
+# Indexes columns of assessment table
+sudo -u postgres psql -d $DBNAME -c "CREATE INDEX idx_assessment_assr_year ON assessment USING btree (assr_year);"
+sudo -u postgres psql -d $DBNAME -c "CREATE INDEX idx_assessment_sa_appraise_yr ON assessment USING btree (sa_appraise_yr);"
+sudo -u postgres psql -d $DBNAME -c "CREATE INDEX idx_assessment_taxyear ON assessment USING btree (taxyear);"
+sudo -u postgres psql -d $DBNAME -c "CREATE INDEX idx_assessment_sa_parcel_nbr_change_yr ON assessment USING btree (sa_parcel_nbr_change_yr);"
---
+++ sql/count_assessment_by_sa_appraise_yr.sql
@@ -0,0 +1,7 @@
+﻿--SELECT sa_appraise_yr, COUNT(sa_appraise_val) FROM assessment WHERE sa_appraise_val IS NOT NULL GROUP BY sa_appraise_yr ORDER BY sa_appraise_yr;
+--SELECT sa_appraise_yr, COUNT(sa_val_market) FROM assessment WHERE sa_val_market IS NOT NULL GROUP BY sa_appraise_yr ORDER BY sa_appraise_yr;
+--SELECT sa_appraise_yr, COUNT(sa_val_assd_land) FROM assessment WHERE sa_val_assd_land IS NOT NULL GROUP BY sa_appraise_yr ORDER BY sa_appraise_yr;
+
+SELECT sa_appraise_yr, COUNT(sa_appraise_val) FROM assessment
+ WHERE sa_appraise_val IS NOT NULL
+ GROUP BY sa_appraise_yr ORDER BY sa_appraise_yr;
