69b363c2106b9761a8520eee022971c5e576f31c
K. Arthur Endsley <endsley@umich.edu>
2016-02-20 17:24:37 -0500

    Updated SQL scripts for joining geocoded addresses to RealtyTrac Recorder data

--- sql/create_geocoded_table_to_be_joined.sql
+++ sql/create_geocoded_table_to_be_joined.sql
@@ -1,4 +1,5 @@
-﻿--Status,Score,Match_type,Match_addr,Side,Ref_ID,X,Y,Addr_type,ARC_Single_Line_Input,property_id,banana,source,rawAddress
+﻿--First run data:
+--Status,Score,Match_type,Match_addr,Side,Ref_ID,X,Y,Addr_type,ARC_Single_Line_Input,property_id,banana,source,rawAddress
 --M,100,A,"11436 Morrish Rd, Gaines, MI, 48436",L,278400301482539,-83.827902,42.857012,StreetAddress,11436 MORRISH RD GAINES MI 48436,133642574,(133642574=> u''=> u''=> u''=> u''=> u'11436 MORRISH RD GAINES MI 48436'),recorder,11436 MORRISH RD GAINES MI 48436
 
 --DROP TABLE geocoded;
@@ -17,5 +18,28 @@ CREATE TABLE geocoded (
    match_info	text,
    source	varchar(255),
    raw_addr	varchar(255))
-
 --COPY geocoded FROM '/home/arthur/Desktop/geocoding_first_run.csv' WITH DELIMITER ',' NULL AS '' CSV HEADER;
+
+--Second run data:
+--X,Y,Status,Score,Match_type,Match_addr,Side,Ref_ID,X,Y,User_fld,Addr_type,ARC_Single_Line_Input,property_id,banana,rawaddress
+--13265234.266076117753983,487432.327099740505219,M,65.28,A,"13092 White Oaks Dr, 48436",R,21143,-83.83769,42.836308,0,StreetAddress,13092 WHITE OAKS GAINES MI 48436,133643084,(133643084=> u''=> u''=> u''=> u''=> u'13092 WHITE OAKS'),13092 WHITE OAKS GAINES MI 48436
+
+--DROP TABLE geocoded_extra;
+CREATE TABLE geocoded_extra (
+   x2		decimal,
+   y2		decimal,
+   status	varchar(1),
+   score	decimal,
+   match_type	varchar(1),
+   match_addr	varchar(255),
+   side		varchar(1),
+   ref_id	bigint,
+   x		decimal,
+   y		decimal,
+   user_fld	varchar(1),
+   addr_type	varchar(255),
+   single_line	varchar(255),
+   property_id	integer,
+   match_info	text,
+   raw_addr	varchar(255))
+--COPY geocoded_extra FROM '/home/arthur/Desktop/geocoding_second_pass.csv' WITH DELIMITER ',' NULL AS '' CSV HEADER;
--- sql/select_transaction_join_geocoded_by_county_and_year.sql
+++ sql/select_transaction_join_geocoded_by_county_and_year.sql
@@ -1,3 +1,18 @@
-﻿SELECT * FROM transaction
- WHERE sr_date_transfer >= 19940101 AND sr_date_transfer < 19950101
-   AND mm_fips_muni_code = 163; -- Wayne County
+﻿SELECT t.sr_property_id, gc.score, t.sr_val_transfer, t.sr_tax_transfer, t.sr_full_part_code,
+       left(t.sr_date_transfer::text, 4) AS transfer_year, left(t.sr_date_filing::text, 4) AS filing_year, 
+       ST_Transform(ST_SetSRID(ST_MakePoint(gc.x, gc.y), 4326), 32617) AS geom
+  INTO temp_transactions_by_year
+  FROM transaction t
+  JOIN geocoded gc ON t.sr_property_id = gc.property_id
+ WHERE gc.score >= 80 AND t.sr_val_transfer > 0
+   --AND t.sr_date_transfer >= 19940101 AND t.sr_date_transfer < 19950101
+   --AND t.mm_fips_muni_code = 163 -- Wayne County 
+
+ UNION
+SELECT t.sr_property_id, gc2.score, t.sr_val_transfer, t.sr_tax_transfer, t.sr_full_part_code,
+       left(t.sr_date_transfer::text, 4) AS transfer_year, left(t.sr_date_filing::text, 4) AS filing_year, 
+       ST_Transform(ST_SetSRID(ST_MakePoint(gc2.x, gc2.y), 4326), 32617) AS geom
+  FROM transaction t
+  JOIN geocoded_extra gc2 ON t.sr_property_id = gc2.property_id
+ WHERE gc2.score > 0 AND t.sr_val_transfer > 0
+   --AND t.mm_fips_muni_code = 163; -- Wayne County 
