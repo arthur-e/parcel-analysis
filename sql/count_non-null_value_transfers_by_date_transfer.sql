86da454964770780a24168047a36f233be802232
K. Arthur Endsley <endsley@umich.edu>
2016-02-21 10:17:33 -0500

    Bug fix and name changes in SQL scripts

--- sql/count_non-null_value_transfers_by_date_transfer.sql
+++ sql/count_non-null_value_transfers_by_date_transfer.sql
@@ -1,4 +1,4 @@
 ﻿SELECT sr_date_transfer, COUNT(sr_val_transfer)
-  FROM record
+  FROM transactions
  WHERE sr_val_transfer IS NOT NULL
  GROUP BY sr_date_transfer ORDER BY sr_date_transfer;
--- sql/select_transaction_join_geocoded_by_county_and_year.sql
+++ sql/select_transaction_join_geocoded_by_county_and_year.sql
@@ -3,7 +3,7 @@ SELECT t.sr_property_id, gc.score, t.sr_val_transfer, t.sr_tax_transfer, t.sr_fu
        left(t.sr_date_transfer::text, 4) AS transfer_year, left(t.sr_date_filing::text, 4) AS filing_year, 
        ST_Transform(ST_SetSRID(ST_MakePoint(gc.x, gc.y), 4326), 32617) AS geom
   --INTO _temp_transactions_by_year_
-  FROM transaction t
+  FROM transactions t
   JOIN geocoded gc ON t.sr_property_id = gc.property_id
  WHERE gc.score >= 80 AND t.sr_val_transfer > 0
    --AND t.sr_date_transfer >= 19940101 AND t.sr_date_transfer < 19950101
@@ -13,7 +13,7 @@ SELECT t.sr_property_id, gc.score, t.sr_val_transfer, t.sr_tax_transfer, t.sr_fu
 SELECT t.sr_property_id, gc2.score, t.sr_val_transfer, t.sr_tax_transfer, t.sr_full_part_code,
        left(t.sr_date_transfer::text, 4) AS transfer_year, left(t.sr_date_filing::text, 4) AS filing_year, 
        ST_Transform(ST_SetSRID(ST_MakePoint(gc2.x, gc2.y), 4326), 32617) AS geom
-  FROM transaction t
+  FROM transactions t
   JOIN geocoded_extra gc2 ON t.sr_property_id = gc2.property_id
  WHERE gc2.score > 0 AND t.sr_val_transfer > 0
    --AND t.mm_fips_muni_code = 163; -- Wayne County 
