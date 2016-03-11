7fb05952b33a57b323d5097adb3dff316afcc733
K. Arthur Endsley <endsley@umich.edu>
2016-03-10 22:00:41 -0500

    Added SQL scripts for geocoding, summarizing the foreclosure data

---
+++ sql/select_002_foreclosure_summarize_foreclosures_by_block_group.sql
@@ -0,0 +1,7 @@
+﻿-- Summarizes foreclosures by year and by block group.
+SELECT bg.geoid10 AS fips, fc.recording_year,
+       count(DISTINCT fc.unique_id_notice)
+  INTO foreclosures_by_block_group
+  FROM census2010_block_groups bg, foreclosures_geocoded fc
+ WHERE ST_ContainsProperly(bg.geom, fc.geom)
+ GROUP BY bg.geoid10, fc.recording_year;
