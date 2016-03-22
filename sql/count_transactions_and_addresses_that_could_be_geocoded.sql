fe4ac9d5a6b6670c9865db92b499ae3f02e35519
K. Arthur Endsley <endsley@umich.edu>
2016-03-22 11:59:10 -0400

    Added a geocoding diagnostic to a SQL script

--- sql/count_transactions_and_addresses_that_could_be_geocoded.sql
+++ sql/count_transactions_and_addresses_that_could_be_geocoded.sql
@@ -23,17 +23,12 @@ SELECT gc.property_id AS sa_property_id FROM
 INTERSECT
 SELECT sa_property_id FROM assessments) sq;
 
--- Number of (non-zero) transactions with an address that could be geocoded:
-SELECT COUNT(tr.sr_unique_id) FROM transactions tr
- WHERE 
-       --tr.sr_val_transfer > 0 AND
-        tr.sr_property_id IN (
+-- Addresses that were not included in Eric's geocoding...
+SELECT * FROM transactions tr
+ WHERE tr.sr_property_id NOT IN (
  SELECT gc.property_id AS sr_property_id FROM
 (SELECT property_id FROM geocoded
-  --WHERE score > 80
   UNION
- SELECT property_id FROM geocoded_extra
-  --WHERE score > 0
-  ) gc
+ SELECT property_id FROM geocoded_extra) gc
  INTERSECT
  SELECT sr_property_id FROM transactions);
