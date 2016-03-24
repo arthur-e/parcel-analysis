c3b5c823e84091173319cabe512bffbe9d55c257
K. Arthur Endsley <endsley@umich.edu>
2016-03-24 11:42:04 -0400

    Modified SQL scripts for summarizing the foreclosure data

--- sql/select_002_foreclosure_summarize_foreclosures_by_block_group.sql
+++ sql/select_002_foreclosure_summarize_foreclosures_by_block_group.sql
@@ -1,7 +1,7 @@
 ﻿-- Summarizes foreclosures by year and by block group.
-SELECT bg.geoid10 AS fips, fc.recording_year,
+SELECT bg.geoid10 AS fips, fc.recording_year, fc.mm_fips_county_name, fc.mm_fips_muni_code,
        count(DISTINCT fc.unique_id_notice)
   INTO foreclosures_by_block_group
   FROM census2010_block_groups bg, foreclosures_geocoded fc
  WHERE ST_ContainsProperly(bg.geom, fc.geom)
- GROUP BY bg.geoid10, fc.recording_year;
+ GROUP BY fc.mm_fips_county_name, fc.mm_fips_muni_code, bg.geoid10, fc.recording_year;
