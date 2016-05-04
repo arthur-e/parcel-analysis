59baca9f0f46ff0b912c8ce60b7cb7ce965af03c
K. Arthur Endsley <endsley@umich.edu>
2016-05-04 15:30:44 -0400

    Changes to SQL scripts summarizing the Assessor's data by block group, tract

--- sql/select_003c_summarize_parcel_and_lot_areas_by_tract.sql
+++ sql/select_003c_summarize_parcel_and_lot_areas_by_tract.sql
@@ -1,4 +1,4 @@
-﻿SELECT bg.geoid10 AS fips,
+﻿SELECT tc.geoid10 AS fips,
        avg(a.lot_size_sqft) AS lot_size_sqft_mean, 
        median(a.lot_size_sqft) AS lot_size_sqft_median, 
        stddev(a.lot_size_sqft) AS lot_size_sqft_sd,
@@ -12,7 +12,7 @@
        median(a.net_unbuilt_area_sqft) AS net_unbuilt_area_sqft_median,
        stddev(a.net_unbuilt_area_sqft) AS net_unbuilt_area_sqft_sd,
        sum(a.net_unbuilt_area_sqft) AS net_unbuilt_area_sqft_total--, a.year_built, a.year_improved
-  INTO assessments_static_by_block_group
-  FROM census2010_block_groups bg, assessments_parcel_characteristics_geocoded a
- WHERE ST_ContainsProperly(bg.geom, a.geom)
- GROUP BY bg.geoid10--, a.year_improved; -- Assume that improvement is pool, garage
+  INTO assessments_static_by_tract
+  FROM census2010_tracts tc, assessments_parcel_characteristics_geocoded a
+ WHERE ST_ContainsProperly(tc.geom, a.geom)
+ GROUP BY tc.geoid10--, a.year_improved; -- Assume that improvement is pool, garage
