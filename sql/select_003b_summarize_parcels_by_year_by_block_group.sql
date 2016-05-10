3ffc3e985ae16ff6cf3c438d9ba19a9f3ad1f0b0
K. Arthur Endsley <endsley@umich.edu>
2016-05-10 08:29:21 -0400

    Added scripts for summairizing new housing starts by Census districts

--- sql/select_003b_summarize_parcels_by_year_by_block_group.sql
+++ sql/select_003b_summarize_parcels_by_year_by_block_group.sql
@@ -1,6 +1,4 @@
 ﻿SELECT bg.geoid10 AS fips,
-       sum(a.pool_present) AS pool_count,
-       sum(a.pool_present) / count(*) AS pool_proportion, 
        count(*) AS new_housing_starts,
        a.year_built--, a.year_improved
   INTO assessments_by_block_group
---
+++ sql/select_003d_summarize_parcels_by_year_by_tract.sql
@@ -0,0 +1,7 @@
+﻿SELECT tc.geoid10 AS fips,
+       count(*) AS new_housing_starts,
+       a.year_built--, a.year_improved
+  INTO assessments_by_tract
+  FROM census2010_tracts tc, assessments_parcel_characteristics_geocoded a
+ WHERE ST_ContainsProperly(tc.geom, a.geom)
+ GROUP BY tc.geoid10, a.year_built-- a.year_improved;
