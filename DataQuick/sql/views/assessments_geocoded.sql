590db841de6a1f74e01bda92df11c86744a7ae14
K. Arthur Endsley <endsley@umich.edu>
2017-03-08 10:28:48 -0500

    Added assessments geocoding, summary scripts
    Added SQL scripts linking assessments to 2000 Census tracts through geocoding.

---
+++ DataQuick/sql/views/assessments_geocoded.sql
@@ -0,0 +1,6 @@
+﻿CREATE OR REPLACE VIEW assessments_geocoded AS (
+SELECT a.*,
+       -- NOTE: DataQuick geocodes stored positive West longitudes...
+       ST_Transform(ST_SetSRID(ST_MakePoint(-gc.sa_x_coord, gc.sa_y_coord), 4326), 26911) AS geom
+  FROM assessments a
+  JOIN geocoding_index gc ON a.sa_property_id = gc.sa_property_id)
---
+++ DataQuick/sql/views/assessments_geocoded_adjusted.sql
@@ -0,0 +1,16 @@
+﻿-- Creates a spatially explicit table of RESIDENTIAL sale prices adjusted to 2010 dollars
+CREATE OR REPLACE VIEW assessments_geocoded_adjusted AS
+SELECT a.use_code_std, a.sa_sqft, sq.*
+ FROM (SELECT a.*, 
+       -- CPI-U adjustment to 2010 dollars
+       sa_val_assd * cpi.adjustment AS sa_val_assd_adj, 
+       sa_val_assd_land * cpi.adjustment AS sa_val_assd_land_adj,
+       sa_val_assd_improv * cpi.adjustment AS sa_val_assd_improv_adj,
+       sa_appraise_val * cpi.adjustment AS sa_appraise_val_adj,
+       sa_val_appraise_land * cpi.adjustment AS sa_val_appraise_land_adj,
+       sa_val_appraise_improv * cpi.adjustment AS sa_val_appraise_improv_adj,
+       sa_val_market * cpi.adjustment AS sa_val_market_adj
+  FROM assessments_geocoded a
+  -- NOTE: About 225,000 properties have a null or "0" appraisal year...
+  JOIN cpi_u_housing_2010 cpi ON a.sa_appraise_yr = cpi.year::text) AS sq
+ WHERE a.use_code_std LIKE 'R%'; -- Residential properties only
