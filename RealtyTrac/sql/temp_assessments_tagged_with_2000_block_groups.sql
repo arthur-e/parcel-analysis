80864ed55ac76e051e4eaac31fde69112839dcbf
K. Arthur Endsley <endsley@umich.edu>
2017-04-18 14:57:48 -0400

    Minor changes to RealtyTrac SQL scripts to improve generality

---
+++ RealtyTrac/sql/temp_assessments_tagged_with_2000_block_groups.sql
@@ -0,0 +1,5 @@
+﻿-- Assessments summarized by year and 2000 block group
+SELECT bg.bkgpidfp00 AS FIPS, asm.*
+  INTO _temp_assessments_tagged_with_2000_block_groups_
+  FROM assessments_parcel_characteristics_geocoded AS asm, census2000_block_groups AS bg
+ WHERE ST_Intersects(bg.geom, asm.geom);
--- RealtyTrac/sql/transactions_by_year_adjusted.sql
+++ RealtyTrac/sql/transactions_by_year_adjusted.sql
@@ -1,9 +1,8 @@
 ﻿-- Creates a spatially explicit table of sale prices adjusted to 2010 dollars
 CREATE TABLE transactions_by_year_adjusted AS
-SELECT a.use_code_std, sq.sr_property_id, sq.score, sq.mm_fips_county_name,
+SELECT a.use_code_std, a.sa_sqft, sq.sr_property_id, sq.score, sq.mm_fips_county_name,
        sq.sr_val_transfer_adj, sq.sr_tax_transfer_adj, sq.sr_date_transfer,
        sq.sr_full_part_code, sq.transfer_year, sq.filing_year, sq.geom
-       --INTO _temp_transactions_by_year_adjusted_
  FROM (SELECT sr_property_id, score, mm_fips_county_name, 
        sr_val_transfer * cpi.adjustment AS sr_val_transfer_adj, 
        sr_tax_transfer * cpi.adjustment AS sr_tax_transfer_adj, 
