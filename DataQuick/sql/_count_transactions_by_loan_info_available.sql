f37cac02e64dc6a0fdf444009194ce62209a3685
K. Arthur Endsley <endsley@umich.edu>
2017-05-15 14:36:33 -0400

    Added loan information to transactions queries

---
+++ DataQuick/sql/_count_transactions_by_loan_info_available.sql
@@ -0,0 +1,25 @@
+﻿SELECT * FROM (
+SELECT 'sr_loan_val_1' AS field, count(*) AS num_valid
+  FROM transactions_geocoded_la_by_census2010_tracts
+ WHERE sr_loan_val_1 > 0 AND sr_loan_val_1 IS NOT NULL
+UNION
+SELECT 'sr_loan_type_1' AS field, count(*) AS num_valid
+  FROM transactions_geocoded_la_by_census2010_tracts
+ WHERE NOT sr_loan_type_1 = '' AND sr_loan_type_1 IS NOT NULL
+UNION
+SELECT 'sr_loan_val_2' AS field, count(*) AS num_valid
+  FROM transactions_geocoded_la_by_census2010_tracts
+ WHERE sr_loan_val_2 > 0 AND sr_loan_val_2 IS NOT NULL
+UNION
+SELECT 'sr_loan_type_2' AS field, count(*) AS num_valid
+  FROM transactions_geocoded_la_by_census2010_tracts
+ WHERE NOT sr_loan_type_2 = '' AND sr_loan_type_2 IS NOT NULL
+UNION
+SELECT 'sr_loan_val_3' AS field, count(*) AS num_valid
+  FROM transactions_geocoded_la_by_census2010_tracts
+ WHERE sr_loan_val_3 > 0 AND sr_loan_val_3 IS NOT NULL
+UNION
+SELECT 'sr_loan_type_3' AS field, count(*) AS num_valid
+  FROM transactions_geocoded_la_by_census2010_tracts
+ WHERE NOT sr_loan_type_3 = '' AND sr_loan_type_3 IS NOT NULL) sq
+ORDER BY field;
--- DataQuick/sql/views/transactions_geocoded_adjusted.sql
+++ DataQuick/sql/views/transactions_geocoded_adjusted.sql
@@ -4,7 +4,10 @@ SELECT a.use_code_std, a.sa_sqft, sq.*
  FROM (SELECT tr.*, 
        -- CPI-U adjustment to 2010 dollars
        sr_val_transfer * cpi.adjustment AS sr_val_transfer_adj, 
-       sr_tax_transfer * cpi.adjustment AS sr_tax_transfer_adj
+       sr_tax_transfer * cpi.adjustment AS sr_tax_transfer_adj,
+       sr_loan_val_1 *  cpi.adjustment AS sr_loan_val_1_adj,
+       sr_loan_val_2 *  cpi.adjustment AS sr_loan_val_2_adj,
+       sr_loan_val_3 *  cpi.adjustment AS sr_loan_val_3_adj
   FROM transactions_geocoded tr
   JOIN cpi_u_housing_2010 cpi ON tr.transfer_year = cpi.year::text) AS sq
   JOIN assessments a ON a.sa_property_id = sq.sr_property_id
