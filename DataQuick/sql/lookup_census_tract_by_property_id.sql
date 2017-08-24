61a491ab2bd65497f0822757cc6852c46ebd37dd
K. Arthur Endsley <endsley@umich.edu>
2017-08-24 15:04:44 -0400

    Fixed bug in lookup script that assigns FIPS codes for each Census tract in each year

--- DataQuick/sql/lookup_census_tract_by_property_id.sql
+++ DataQuick/sql/lookup_census_tract_by_property_id.sql
@@ -1,13 +1,13 @@
-﻿SELECT tr.sr_property_id,
+﻿SELECT sq.sr_property_id,
        tracts2010.fips AS fips2010, 
        tracts2000.fips AS fips2000,
        tracts1990.fips AS fips1990
   INTO lookup_census_tract_by_property_id
-  FROM transactions_geocoded_adjusted AS tr, 
-       census2010_tracts AS tracts2010, 
-       census2000_tracts AS tracts2000,
-       census1990_tracts AS tracts1990
- WHERE tr.mm_fips_county_name = 'LOS ANGELES'
-   AND ST_Intersects(tracts2010.geom, tr.geom)
-   AND ST_Intersects(tracts2000.geom, tr.geom)
-   AND ST_Intersects(tracts1990.geom, tr.geom);
+  FROM (
+  SELECT DISTINCT tr.sr_property_id, tr.geom
+    FROM transactions_geocoded AS tr
+   WHERE tr.mm_fips_county_name = 'LOS ANGELES') sq
+
+  JOIN census2010_tracts AS tracts2010 ON ST_Intersects(tracts2010.geom, sq.geom)
+  JOIN census2000_tracts AS tracts2000 ON ST_Intersects(tracts2000.geom, sq.geom)
+  JOIN census1990_tracts AS tracts1990 ON ST_Intersects(tracts1990.geom, sq.geom);
