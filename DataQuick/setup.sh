47bc97d6853b26a969e5913bbc8c156360679e43
K. Arthur Endsley <endsley@umich.edu>
2017-08-28 13:52:13 -0400

    Updated DataQuick data entry scripts

--- DataQuick/setup.sh
+++ DataQuick/setup.sh
@@ -7,73 +7,79 @@ ASSESSOR_DATA_DICT=$DQ_DATA_DIR/20121127_UNIVMI_ASSESSOR_LAYOUT.csv
 RECORDER_DATA_DICT=$DQ_DATA_DIR/20121127_UNIVMI_HISTORY_LAYOUT.csv
 
 # Create the database
-. create_db.sh $DBNAME $OWNER
+# . create_db.sh $DBNAME $OWNER
 
-# Create the record (Recorder) table
-python create_from_data_dict.py "transactions" $RECORDER_DATA_DICT "sr_unique_id" | sudo -u postgres psql -d $DBNAME -f -
-
-# Create the assessment (Assessor Record) table
-python create_from_data_dict.py "assessments" $ASSESSOR_DATA_DICT "sa_property_id" | sudo -u postgres psql -d $DBNAME -f -
-
-sudo -u postgres psql -h localhost -d $DBNAME -f $BASE_DIR/sql/create_CPI_inflation_adjustment_table.sql
+read -n 1 -p "Insert CPI tables into database? [Y/n] " cont
+echo ""
+if [ "$cont" = "Y" ]; then
+  sudo -u postgres psql -h localhost -d $DBNAME -f $BASE_DIR/sql/create_CPI_inflation_adjustment_table.sql
+fi
 
 read -n 1 -p "Insert Shapefiles into spatial database? [Y/n] " cont
 echo ""
 if [ "$cont" = "Y" ]; then
-    # Insert 2010 Census geometries: Downloaded from <https://www.census.gov/geo/maps-data/data/cbf/cbf_tracts.html>
-    shp2pgsql -s 26911 -I $GIS_DIR/LA_County_census_tracts_2010_utm11n.shp public.census2010_tracts | sudo -u postgres psql -h localhost -d $DBNAME
-    sudo -u postgres psql -h localhost -d $DBNAME -c "VACUUM ANALYZE census2010_tracts (geom);"
-    # Add FIPS codes for this table
-    sudo -u postgres psql -h localhost -d $DBNAME -c "ALTER TABLE census2010_tracts ADD COLUMN fips varchar(13);"
-    sudo -u postgres psql -h localhost -d $DBNAME -c "UPDATE census2010_tracts SET fips = state || county || tract;"
+  # Insert 2010 Census geometries: Downloaded from <https://www.census.gov/geo/maps-data/data/cbf/cbf_tracts.html>
+  shp2pgsql -s 26911 -I $GIS_DIR/LA_County_census_tracts_2010_utm11n.shp public.census2010_tracts | sudo -u postgres psql -h localhost -d $DBNAME
+  sudo -u postgres psql -h localhost -d $DBNAME -c "VACUUM ANALYZE census2010_tracts (geom);"
+  # Add FIPS codes for this table
+  sudo -u postgres psql -h localhost -d $DBNAME -c "ALTER TABLE census2010_tracts ADD COLUMN fips varchar(13);"
+  sudo -u postgres psql -h localhost -d $DBNAME -c "UPDATE census2010_tracts SET fips = state || county || tract;"
 
-    # Insert 2000 Census geometries: Downloaded from <https://www.census.gov/geo/maps-data/data/cbf/cbf_tracts.html>
-    shp2pgsql -s 26911 -I $GIS_DIR/LA_County_census_tracts_2000_utm11n.shp public.census2000_tracts | sudo -u postgres psql -h localhost -d $DBNAME
-    sudo -u postgres psql -h localhost -d $DBNAME -c "VACUUM ANALYZE census2000_tracts (geom);"
-    # Add FIPS codes for this table
-    sudo -u postgres psql -h localhost -d $DBNAME -c "ALTER TABLE census2000_tracts ADD COLUMN fips varchar(13);"
-    sudo -u postgres psql -h localhost -d $DBNAME -c "UPDATE census2000_tracts SET fips = state || county || tract;"
+  # Insert 2000 Census geometries: Downloaded from <https://www.census.gov/geo/maps-data/data/cbf/cbf_tracts.html>
+  shp2pgsql -s 26911 -I $GIS_DIR/LA_County_census_tracts_2000_utm11n.shp public.census2000_tracts | sudo -u postgres psql -h localhost -d $DBNAME
+  sudo -u postgres psql -h localhost -d $DBNAME -c "VACUUM ANALYZE census2000_tracts (geom);"
+  # Add FIPS codes for this table
+  sudo -u postgres psql -h localhost -d $DBNAME -c "ALTER TABLE census2000_tracts ADD COLUMN fips varchar(13);"
+  sudo -u postgres psql -h localhost -d $DBNAME -c "UPDATE census2000_tracts SET fips = state || county || tract;"
 
-    # Insert 1990 Census geometries
-    shp2pgsql -s 26911 -I $GIS_DIR/LA_County_census_tracts_1990_utm11n.shp public.census1990_tracts | sudo -u postgres psql -h localhost -d $DBNAME
-    sudo -u postgres psql -h localhost -d $DBNAME -c "VACUUM ANALYZE census1990_tracts (geom);"
-    # Add FIPS codes for this table
-    sudo -u postgres psql -h localhost -d $DBNAME -c "ALTER TABLE census1990_tracts ADD COLUMN fips varchar(13);"
-    sudo -u postgres psql -h localhost -d $DBNAME -c "UPDATE census1990_tracts SET fips = st || co || tract_name;"
+  # Insert 1990 Census geometries
+  shp2pgsql -s 26911 -I $GIS_DIR/LA_County_census_tracts_1990_utm11n.shp public.census1990_tracts | sudo -u postgres psql -h localhost -d $DBNAME
+  sudo -u postgres psql -h localhost -d $DBNAME -c "VACUUM ANALYZE census1990_tracts (geom);"
+  # Add FIPS codes for this table
+  sudo -u postgres psql -h localhost -d $DBNAME -c "ALTER TABLE census1990_tracts ADD COLUMN fips varchar(13);"
+  sudo -u postgres psql -h localhost -d $DBNAME -c "UPDATE census1990_tracts SET fips = st || co || tract_name;"
 
-    # NOTE: As an example... Insert 2010 Census geometries: Downloaded from <https://www.census.gov/geo/maps-data/data/cbf/cbf_tracts.html>
-    # unzip ~/Downloads/gz_2010_06_140_00_500k.zip -d ~/Downloads
-    # ogr2ogr -f "ESRI Shapefile" -where "COUNTY IN ('037', '059', '065', '071', '111')" -s_srs "EPSG:4269" -t_srs "EPSG:26911" $GIS_DIR/LosAngeles/census_tracts_2010_utm11n.shp ~/Downloads/gz_2010_06_140_00_500k/gz_2010_06_140_00_500k.shp
-    # rm -fr ~/Downloads/gz_2010_06_140_00_500k
-    # NOTE: Manually selected LA County ('037') only, and removed outlying islands
+  # NOTE: As an example... Insert 2010 Census geometries: Downloaded from <https://www.census.gov/geo/maps-data/data/cbf/cbf_tracts.html>
+  # unzip ~/Downloads/gz_2010_06_140_00_500k.zip -d ~/Downloads
+  # ogr2ogr -f "ESRI Shapefile" -where "COUNTY IN ('037', '059', '065', '071', '111')" -s_srs "EPSG:4269" -t_srs "EPSG:26911" $GIS_DIR/LosAngeles/census_tracts_2010_utm11n.shp ~/Downloads/gz_2010_06_140_00_500k/gz_2010_06_140_00_500k.shp
+  # rm -fr ~/Downloads/gz_2010_06_140_00_500k
+  # NOTE: Manually selected LA County ('037') only, and removed outlying islands
 fi
 
 read -n 1 -p "Inflate DataQuick data and insert into database? [Y/n] " cont
 echo ""
 if [ "$cont" = "Y" ]; then
-    echo "Inflating data..."
-    unzip $DQ_DATA_DIR/20121127_UNIVMI_ASSESSOR.ZIP -d $DQ_DATA_DIR
-    for file in $(ls $DQ_DATA_DIR/*UNIVMI_HISTORY*.ZIP)
-    do
-        unzip $file -d $DQ_DATA_DIR
-    done
 
-    echo "Converting from fixed width to CSV..."
-    mkdir $DQ_DATA_DIR/csv
-    sh convert_fixed_width_to_csv.sh $ASSESSOR_DATA_DICT 6 "|" $DQ_DATA_DIR/UNIVMI_ASSESSOR.TXT > $DQ_DATA_DIR/csv/Assessor_data_LA.csv
+  echo "Creating tables..."
+  # Create the record (Recorder) table
+  python create_from_data_dict.py "transactions" $RECORDER_DATA_DICT "sr_unique_id" | sudo -u postgres psql -d $DBNAME -f -
 
-    for file in $(ls $DQ_DATA_DIR/*UNIVMI_HISTORY*.TXT)
-    do
-        filename=`expr match $file '.*/\(.*\).TXT'`
-        echo "Converting ${file}.TXT..."
-        sh convert_fixed_width_to_csv.sh $RECORDER_DATA_DICT 6 "|" $file > "$DQ_DATA_DIR/csv/${filename}.csv"
-    done
+  # Create the assessment (Assessor Record) table
+  python create_from_data_dict.py "assessments" $ASSESSOR_DATA_DICT "sa_property_id" | sudo -u postgres psql -d $DBNAME -f -
 
-    echo "Inserting data into database tables..."
-    sudo -u postgres psql -d $DBNAME -c "COPY assessments FROM '$DQ_DATA_DIR/csv/Assessor_data_LA.csv' WITH DELIMITER '|' NULL AS ''"
+  echo "Inflating data..."
+  unzip $DQ_DATA_DIR/20121127_UNIVMI_ASSESSOR.ZIP -d $DQ_DATA_DIR
+  for file in $(ls $DQ_DATA_DIR/*UNIVMI_HISTORY*.ZIP)
+  do
+      unzip $file -d $DQ_DATA_DIR
+  done
 
-    for file in $(ls $DQ_DATA_DIR/csv/*UNIVMI_HISTORY*.csv)
-    do
-        sudo -u postgres psql -d $DBNAME -c "COPY transactions FROM '${file}' WITH DELIMITER '|' NULL AS ''"
-    done
+  echo "Converting from fixed width to CSV..."
+  mkdir $DQ_DATA_DIR/csv
+  sh convert_fixed_width_to_csv.sh $ASSESSOR_DATA_DICT 6 "|" $DQ_DATA_DIR/UNIVMI_ASSESSOR.TXT > $DQ_DATA_DIR/csv/Assessor_data_LA.csv
+
+  for file in $(ls $DQ_DATA_DIR/*UNIVMI_HISTORY*.TXT)
+  do
+      filename=`expr match $file '.*/\(.*\).TXT'`
+      echo "Converting ${file}.TXT..."
+      sh convert_fixed_width_to_csv.sh $RECORDER_DATA_DICT 6 "|" $file > "$DQ_DATA_DIR/csv/${filename}.csv"
+  done
+
+  echo "Inserting data into database tables..."
+  sudo -u postgres psql -d $DBNAME -c "COPY assessments FROM '$DQ_DATA_DIR/csv/Assessor_data_LA.csv' WITH DELIMITER '|' NULL AS ''"
+
+  for file in $(ls $DQ_DATA_DIR/csv/*UNIVMI_HISTORY*.csv)
+  do
+      sudo -u postgres psql -d $DBNAME -c "COPY transactions FROM '${file}' WITH DELIMITER '|' NULL AS ''"
+  done
 fi
--- DataQuick/sql/transactions_geocoded_adjusted.sql
+++ DataQuick/sql/transactions_geocoded_adjusted.sql
@@ -1,14 +1,14 @@
 ﻿-- Creates a spatially explicit table of sale prices adjusted to 2010 dollars
-SELECT a.use_code_std, a.sa_sqft, sq.*
- INTO transactions_geocoded_adjusted
- FROM (SELECT tr.*, 
+SELECT a.use_code_std, a.sa_sqft, tr.*,
        -- CPI-U adjustment to 2010 dollars
        sr_val_transfer * cpi.adjustment AS sr_val_transfer_adj, 
        sr_tax_transfer * cpi.adjustment AS sr_tax_transfer_adj,
        sr_loan_val_1 *  cpi.adjustment AS sr_loan_val_1_adj,
        sr_loan_val_2 *  cpi.adjustment AS sr_loan_val_2_adj,
        sr_loan_val_3 *  cpi.adjustment AS sr_loan_val_3_adj
+  INTO transactions_geocoded_adjusted_los_angeles
   FROM transactions_geocoded tr
-  JOIN cpi_u_housing_2010 cpi ON tr.transfer_year = cpi.year::text) AS sq
-  JOIN assessments a ON a.sa_property_id = sq.sr_property_id;
+  JOIN cpi_u_housing_2010 cpi ON tr.transfer_year = cpi.year::text
+  JOIN assessments a ON a.sa_property_id = tr.sr_property_id
+ WHERE tr.mm_fips_county_name = 'LOS ANGELES';
  --WHERE a.use_code_std LIKE 'R%'; -- Residential properties only
